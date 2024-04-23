b1e5fe17cb64873b1c024959176d6a91394ffa54
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2024-04-23 21:57:12 +0700

    UPPPP

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,12 +1,10 @@
 package server;
-
 import DAOTest.*;
 import DAOTest.impl.*;
 import Entities.KhachHang;
 import Entities.KhuyenMai;
 import Entities.NhanVien;
 import Entities.TaiKhoan;
-
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -19,18 +17,20 @@ import java.util.Hashtable;
 
 public class Server {
 
-    private static final String URL = "rmi://192.168.1.16:6541/";
+    private static final String URL = "rmi://192.168.1.15:6541/";
 
     public static void main(String[] args) throws IOException, NamingException {
         try {
-
-
+            Date ngaySinh = new Date("2003/05/29");
+            String TenDangNhap = "NV111";
+            NhanVien nv = new NhanVien("NV111", "Hoàng Phúc", ngaySinh, "CCCD001", "0123456789",
+                    1, 1, 1, 1, "hinhAnh.png", "email@example.com", "Dia Chi"
+            );
+            TaiKhoan taiKhoan = new TaiKhoan(nv, "123456");
             Hashtable<String, String> env = new Hashtable();
             env.put("java.security.policy", "rmi/policy.policy");
-
-
+            LocateRegistry.createRegistry(6541);
             Context context = new InitialContext();
-
             KhachHangDao khachHangDao = new KhachHangImpl();
             KhuyenMaiDao khuyenMaiDao = new KhuyenMaiImpl();
             LapHoaDonDao lapHoaDonDao = new LapHoaDonImpl();
@@ -42,26 +42,25 @@ public class Server {
             ThongTinCaNhanDao thongTinCaNhanDao = new ThongTinCaNhanImpl();
             TinhTrangSanPhamDao tinhTrangSanPhamDao = new TinhTrangSanPhamImpl();
             XemHoaDonDao xemHoaDonDao = new XemHoaDonImpl();
-
-            LocateRegistry.createRegistry(6541);
-
-
             context.bind(URL + "KhachHangDao", khachHangDao);
             context.bind(URL + "KhuyenMaiDao", khuyenMaiDao);
             context.bind(URL + "LapHoaDonDao", lapHoaDonDao);
             context.bind(URL + "NhaCungCapDao", nhaCungCapDao);
             context.bind(URL + "NhanVienDao", nhanVienDao);
-            context.rebind(URL + "SanPhamDao", sanPhamDao);
+            context.bind(URL + "SanPhamDao", sanPhamDao);
             context.bind(URL + "ThongKeDoanhThuDao", thongKeDoanhThuDao);
             context.bind(URL + "ThongKeHoaDonDao", thongKeHoaDonDao);
             context.bind(URL + "ThongTinCaNhanDao", thongTinCaNhanDao);
             context.bind(URL + "TinhTrangSanPhamDao", tinhTrangSanPhamDao);
             context.bind(URL + "XemHoaDonDao", xemHoaDonDao);
-
+            if (nhanVienDao.getAllNV().size() == 0) {
+                nhanVienDao.them("NV111", "Hoàng Phúc", ngaySinh, "CCCD001", "0123456789",
+                        1, 1, 1, 1, "hinhAnh.png", "email@example.com", "Dia Chi");
+                nhanVienDao.addTaiKhoan(TenDangNhap);
+            }
             System.out.println("Server is running...");
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
